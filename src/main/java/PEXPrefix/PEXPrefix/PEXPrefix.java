0793d3efa11a2cfd01c035d2bdeb9c6de302d82d
Soerxpso <soerxpso@gmail.com>
2016-01-25 23:01:14 -0800

    Initial commit

---
+++ src/main/java/PEXPrefix/PEXPrefix/PEXPrefix.java
@@ -0,0 +1,20 @@
+package PEXPrefix.PEXPrefix;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
+public final class PEXPrefix extends JavaPlugin implements Listener {
+
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent e) {
+		e.setFormat("[" + PermissionsEx.getUser(e.getPlayer()).getPrefix() + "]<%1$s>%1$s");
+	}
+}
